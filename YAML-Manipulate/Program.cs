59c498965eb0ea2c8edb9c786ab321da8502eca9
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-02-04 18:26:16 +0530

    Deserialize to objects

--- YAML-Manipulate/Program.cs
+++ YAML-Manipulate/Program.cs
@@ -7,15 +7,20 @@ namespace YAML_Manipulate
     {
         public static void Main(String[] args)
         {
-            /// Load yaml string as a stream
-            //var loadYamlStream = new LoadingYamlStream();
-            //loadYamlStream.Run();
+            //Load yaml string as a stream
+            /*var loadYamlStream = new LoadingYamlStream();
+            loadYamlStream.Run();*/
 
-            //var loadingYamlFileAsStream = new LoadingYamlFileAsStream();
-            //loadingYamlFileAsStream.Run();
+            /*var loadingYamlFileAsStream = new LoadingYamlFileAsStream();
+            loadingYamlFileAsStream.Run();*/
 
-            var convertYamlToJson = new ConvertYamlToJson();
-            convertYamlToJson.Run();
+            /*var convertYamlToJson = new ConvertYamlToJson();
+            convertYamlToJson.Run();*/
+
+            var deserializeToObject = new DeserializeToObject();
+            deserializeToObject.Run();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ YamlDotNetSamples/DeserializeToObject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace YamlDotNetSamples
+{
+    public class DeserializeToObject
+    {
+
+        public void Run()
+        {
+            string input = "";
+
+            using (var sr = new StreamReader(@"E:\Projects\Practice\DOT NET\YAML-Manipulate\YamlDotNetSamples\student.yaml")) {
+                input = sr.ReadToEnd();
+                Console.WriteLine(input);
+            }
+
+            var deserializer = new DeserializerBuilder().Build();
+
+            var student = deserializer.Deserialize<Student>(input);
+
+            //Console.WriteLine(student.name);
+
+            foreach (var subject in student.subjects)
+            {
+                Console.WriteLine(subject.topic);
+            }
+        }
+    }
+
+    public class Student
+    {
+        public string name { get; set; }
+        public string email{ get; set; }
+        public string mobile { get; set; }
+        public List<Subject> subjects { get; set; }
+    }
+
+    public class Subject
+    {
+        public string topic { get; set; }
+    }
+
+    
+}
