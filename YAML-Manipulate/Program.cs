be4f95ca61c608b91152e466c07afeb8eb6676ee
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-02-04 14:29:26 +0530

    Code refactored

--- YAML-Manipulate/Program.cs
+++ YAML-Manipulate/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using YamlDotNet.RepresentationModel;
-using Xunit.Abstractions;
+using YamlDotNetSamples;
 
 namespace YAML_Manipulate
 {
@@ -13,65 +7,9 @@ namespace YAML_Manipulate
     {
         public static void Main(String[] args)
         {
-            var input = new StringReader(Document);
-
-            var yaml = new YamlStream();
-            yaml.Load(input);
-
-            var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-
-            foreach(var entry in mapping.Children)
-            {
-                Console.WriteLine(((YamlScalarNode)entry.Key).Value);
-            }
-
-            var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("items")];
-
-            foreach (YamlMappingNode item in items)
-            {
-                Console.WriteLine(
-                    "{0}\t{1}",
-                    item.Children[new YamlScalarNode("part_no")],
-                    item.Children[new YamlScalarNode("descrip")]
-                );
-            }
-
-            Console.ReadLine();
+            var loadYamlStream = new LoadingYamlStream();
+            loadYamlStream.Run();
         }
-
-        private const string Document = @"---
-            receipt:    Oz-Ware Purchase Invoice
-            date:        2007-08-06
-            customer:
-                given:   Dorothy
-                family:  Gale
-
-            items:
-                - part_no:   A4786
-                  descrip:   Water Bucket (Filled)
-                  price:     1.47
-                  quantity:  4
-
-                - part_no:   E1628
-                  descrip:   High Heeled ""Ruby"" Slippers
-                  price:     100.27
-                  quantity:  1
-
-            bill-to:  &id001
-                street: |
-                        123 Tornado Alley
-                        Suite 16
-                city:   East Westville
-                state:  KS
-
-            ship-to:  *id001
-
-            specialDelivery:  >
-                Follow the Yellow Brick
-                Road to the Emerald City.
-                Pay no attention to the
-                man behind the curtain.
-                ...";
     }
 }
 
